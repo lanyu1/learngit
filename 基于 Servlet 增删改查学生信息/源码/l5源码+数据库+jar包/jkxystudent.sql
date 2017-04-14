ec5d18675a0713da31fb904c442376f3cf84c5a2
lanyu1 <1563274504@qq.com>
2017-04-14 10:27:17 +0800

    commit git

---
+++ 基于 Servlet 增删改查学生信息/源码/l5源码+数据库+jar包/jkxystudent.sql
@@ -0,0 +1,54 @@
+/*
+SQLyog 企业版 - MySQL GUI v7.14 
+MySQL - 5.6.21-log : Database - jkxystudent
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`jkxystudent` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci */;
+
+USE `jkxystudent`;
+
+/*Table structure for table `studentinfo` */
+
+DROP TABLE IF EXISTS `studentinfo`;
+
+CREATE TABLE `studentinfo` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nicheng` char(10) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `truename` char(10) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `xb` bit(1) DEFAULT NULL,
+  `csrq` date DEFAULT NULL,
+  `zy` char(10) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `kc` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `xq` char(20) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `bz` varchar(500) COLLATE utf8_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Data for the table `studentinfo` */
+
+insert  into `studentinfo`(`id`,`nicheng`,`truename`,`xb`,`csrq`,`zy`,`kc`,`xq`,`bz`) values (2,'李四','李晓兰','\0','1998-09-09','通信工程','数据结构&操作系统','book&web','我非常喜欢计算机'),(3,NULL,NULL,'',NULL,'通信工程','','',NULL),(4,'张四','李晓红','\0','1998-09-06','电气工程','数据库原理','','我有点喜欢这门课程'),(5,NULL,NULL,'',NULL,'通信工程','','',NULL);
+
+/*Table structure for table `usertable` */
+
+DROP TABLE IF EXISTS `usertable`;
+
+CREATE TABLE `usertable` (
+  `username` char(10) COLLATE utf8_unicode_ci NOT NULL,
+  `password` char(10) COLLATE utf8_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Data for the table `usertable` */
+
+insert  into `usertable`(`username`,`password`) values ('11','11'),('22','22');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
---
+++ 基于 Servlet 增删改查学生信息/源码/l5源码+数据库+jar包/jkxystudent2/src/com/jkxy/servlet/inputStuInfoServlet.java
@@ -0,0 +1,101 @@
+package com.jkxy.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.jkxy.model.stuInfo;
+import com.jkxy.service.stuInfoService;
+
+public class inputStuInfoServlet extends HttpServlet {
+
+	/**
+	 * Constructor of the object.
+	 */
+	public inputStuInfoServlet() {
+		super();
+	}
+
+	/**
+	 * Destruction of the servlet. <br>
+	 */
+	public void destroy() {
+		super.destroy(); // Just puts "destroy" string in log
+		// Put your code here
+	}
+
+	/**
+	 * The doGet method of the servlet. <br>
+	 *
+	 * This method is called when a form has its tag value method equals to get.
+	 * 
+	 * @param request the request send by the client to the server
+	 * @param response the response send by the server to the client
+	 * @throws ServletException if an error occurred
+	 * @throws IOException if an error occurred
+	 */
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		doPost(request, response);
+	}
+
+	/**
+	 * The doPost method of the servlet. <br>
+	 *
+	 * This method is called when a form has its tag value method equals to post.
+	 * 
+	 * @param request the request send by the client to the server
+	 * @param response the response send by the server to the client
+	 * @throws ServletException if an error occurred
+	 * @throws IOException if an error occurred
+	 */
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+        request.setCharacterEncoding("utf-8");
+		String nicheng=request.getParameter("nicheng");
+		String truename=request.getParameter("truename");
+		byte xb=Byte.parseByte(request.getParameter("xb"));
+		String csrq=request.getParameter("csrq");
+		String zy=request.getParameter("zy");
+		System.out.println(zy);
+		//String kc=request.getParameter("kc");
+		String kcs[]=request.getParameterValues("kc");
+		String xqs[]=request.getParameterValues("xq");
+		String bz=request.getParameter("bz");
+		stuInfo stu=new stuInfo();
+		stu.setNicheng(nicheng);
+		stu.setTruename(truename);
+		
+		stu.setCsrq(csrq);
+		if(csrq.equals(""))
+			stu.setCsrq(null);
+		if(kcs!=null)
+		stu.setKc(kcs);
+		if(xqs!=null)
+		stu.setXq(xqs);
+		stu.setBz(bz);
+		stu.setZy(zy);
+		stu.setXb(xb);
+		if(new stuInfoService().addStu(stu))
+			response.sendRedirect("../inputStuInfo_success.jsp");
+		else
+			response.sendRedirect("../inputStuInfo.jsp");
+		
+	}
+
+	/**
+	 * Initialization of the servlet. <br>
+	 *
+	 * @throws ServletException if an error occurs
+	 */
+	public void init() throws ServletException {
+		// Put your code here
+	}
+
+}
